db42d27f05a0bb633cfe9c58bff754d669cdedc0
Boringtao <taoxh.uk@gmail.com>
2021-12-18 04:54:05 +0000

    Add: moving async client

--- src/omniverse/setup.py
+++ src/omniverse/setup.py
@@ -23,7 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'bootstrap = omniverse.bootstrap:main'
+            'bootstrap = omniverse.bootstrap:main',
+            'client_async = omniverse.client_async:main'
         ],
     },
 )
